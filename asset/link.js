f931e81c579abe7eb5d2be9e630819a7905c9f6d
Andika Pranayoga <praandikayoga@gmail.com>
2023-04-25 13:56:21 +0800

    Update link wa

--- asset/link.js
+++ asset/link.js
@@ -1,5 +1,5 @@
 let link = document.querySelectorAll(".linkWa");
 
 for (let i = 0; i < link.length; i++) {
-    link[i].setAttribute("href", "https://wa.me/6281246571421?text=Halo%20saya%20ingin%20info%20motor%20CBU%0A%0Ahttps://cbu.yamahabismagroup.com");
+    link[i].setAttribute("href", "https://wa.me/6281999482962?text=Halo%20saya%20ingin%20info%20motor%20CBU%0A%0Ahttps://cbu.yamahabismagroup.com");
 }
