48834fecf3e7b7c61c137134f31c0326f94d5cdc
Andika Pranayoga <praandikayoga@gmail.com>
2023-04-26 09:23:13 +0800

    Create Link WA Model

--- asset/link-wa.js
+++ asset/link-wa.js
@@ -1,5 +1,15 @@
 let link = document.querySelectorAll(".linkWa");
+let page = window.location.pathname;
 
-for (let i = 0; i < link.length; i++) {
-    link[i].setAttribute("href", "https://wa.me/6281999482962?text=Halo%20saya%20ingin%20info%20motor%20CBU%0A%0Ahttps://www.cbu.yamahabismagroup.com/");
+if (page == '/product.php') {
+    let model = document.getElementById("model");
+    for (let i = 0; i < link.length; i++) {
+        link[i].setAttribute("href", `https://wa.me/6281999482962?text=Halo%20saya%20ingin%20info%20motor%20${model.textContent}%0A%0Ahttps://www.cbu.yamahabismagroup.com/`);
+    }
+} else {
+    for (let i = 0; i < link.length; i++) {
+        link[i].setAttribute("href", "https://wa.me/6281999482962?text=Halo%20saya%20ingin%20info%20motor%20CBU%0A%0Ahttps://www.cbu.yamahabismagroup.com/");
+    }
 }
+
+console.log(page, model.textContent);
--- component/model.php
+++ component/model.php
@@ -5,28 +5,28 @@
                 ?>
                     <img src="img/yz125x.png" class="img-fluid">
                     <div class="model-name">
-                        <a>Yamaha YZ125X</a>
+                        <a>Yamaha <span id="model">YZ125X</span></a>
                     </div>
                 <?php
             } elseif ($_GET['model'] == 'yz250x') {
                 ?>
                     <img src="img/yz250x.png" class="img-fluid">
                     <div class="model-name">
-                        <a>Yamaha YZ250X</a>
+                        <a>Yamaha <span id="model">YZ250X</span></a>
                     </div>
                 <?php
             } elseif ($_GET['model'] == 'yz250f') {
                 ?>
                     <img src="img/yz250f.png" class="img-fluid">
                     <div class="model-name">
-                        <a>Yamaha YZ250F</a>
+                        <a>Yamaha <span id="model">YZ250F</span></a>
                     </div>
                 <?php
             } else {
                 ?>
                     <img src="img/yz250fx.png" class="img-fluid">
                     <div class="model-name">
-                        <a>Yamaha YZ250FX</a>
+                        <a>Yamaha <span id="model">YZ250FX</span></a>
                     </div>
                 <?php
             }
